0d5e8f3516a6197336d432c6a6e355086de5d260
ry24-1 <ry24_1@yeah.net>
2025-06-02 13:37:37 +0800

    Delete resources directory

--- resources/js/index.js
+++
@@ -1,9 +0,0 @@
-function replace(){
-	var title_bar_height=document.getElementById("title_bar").offsetHeight;
-	// console.log(title_bar_height);
-	document.getElementById("top_logo").style.height=title_bar_height+"px";
-}
-document.addEventListener("DOMContentLoaded",replace)
-
-
-
